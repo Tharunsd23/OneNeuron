087c22dd6d64019d6533fdcd986d52ce4523c473
Tharunsd23 <tharunsd23@gmail.com>
2021-09-25 12:20:59 +0530

    docstring updated

--- or.py
+++ or.py
@@ -9,7 +9,8 @@ logging.basicConfig(level=logging.INFO,format=logging_str)
 log_dir = "logss"
 os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
-def main(data,eta, epochs,filename):
+
+def main(data,eta, modelName, epochs,plotName):
 
     df= pd.DataFrame(data)
     logging.info(f"This is actual dataframe{df}")
@@ -21,16 +22,23 @@ def main(data,eta, epochs,filename):
     model.fit(X,y)
     _= model.total_loss()
 
-    save_model(model,filename)
-    save_plot(df,"and.png",model)
+    save_model(model,filename=modelName)
+    save_plot(df,plotName,model)
 
 if __name__ == '__main__':
    
-    AND = {
+    OR= {
         "x1": [0,0,1,1],
         "x2": [0,1,0,1],
-        "y": [0,0,0,1],
+        "y": [0,1,1,1],
     }
     ETA = 0.3
     EPOCHS = 10
-    main(data=AND,eta=ETA,epochs=EPOCHS,filename="and.model")
+    try:
+        logging.info(">>>>> starting training >>>>>")
+        main(data=OR, modelName="or.model", plotName="or.png", eta=ETA, epochs=EPOCHS)
+        logging.info("<<<<< training done successfully<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
+   
--- utils/model.py
+++ utils/model.py
@@ -1,8 +1,10 @@
 import numpy as np
-
+import logging
+from tqdm import tqdm
 class Perceptron:
   def __init__(self,eta,epochs):
     self.weights= np.random.randn(3)* 1e-4
+    logging.info(f"initial weights before training: \n {self.weights}")
     self.eta = eta #learning rate
     self.epochs = epochs 
   
@@ -15,26 +17,26 @@ class Perceptron:
     self.y = y 
 
     X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] #Forward propogation
-    print(f"X with bias: \n{X_with_bias}")
+    logging.info(f"X with bias: \n{X_with_bias}")
 
-    for epoch in range(self.epochs):
-      print("--"*10)
-      print(f"for epoch:{epoch}")
-      print("--"*10)
+    for epoch in tqdm(range(self.epochs),total=self.epochs,desc="training the model"):
+      logging.info("--"*10)
+      logging.info(f"for epoch:{epoch}")
+      logging.info("--"*10)
 
       yhat = self.activationFucnction(X_with_bias,self.weights)
-      print(f"predicted value after forward pass: \n{yhat}")
+      logging.info(f"predicted value after forward pass: \n{yhat}")
       self.error = self.y - yhat
-      print(f"error: \n{self.error}")
+      logging.info(f"error: \n{self.error}")
       self.weights = self.weights + self.eta * np.dot(X_with_bias.T,self.error) #Backward propogation
-      print(f"updated weights after epoch:\n{epoch}/{self.epochs} : \n{self.weights}") 
-      print("#####"*10)
+      logging.info(f"updated weights after epoch:\n{epoch}/{self.epochs} : \n{self.weights}") 
+      logging.info("#####"*10)
   def predict(self,X):
     X_with_bias = np.c_[X, - np.ones((len(X),1))]
     return self.activationFucnction(X_with_bias,self.weights)
   
   def total_loss(self):
     total_loss = np.sum(self.error)
-    print(f"total loss:{total_loss}")
+    logging.info(f"total loss:{total_loss}")
     return total_loss
   
