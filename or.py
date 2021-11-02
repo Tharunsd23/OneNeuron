21c9bbefee0716bfe9c612dd6098f75d6a20c45a
Tharunsd23 <tharunsd23@gmail.com>
2021-11-02 15:20:51 +0530

    docstring updated

--- or.py
+++ or.py
@@ -5,7 +5,6 @@ import numpy as np
 import logging
 import os
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
-logging.basicConfig(level=logging.INFO,format=logging_str)
 log_dir = "logss"
 os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
